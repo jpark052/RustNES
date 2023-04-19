c68d87e0d989bf55495eba7f347365d2c51a30df
Jae Park <jaepark052@gmail.com>
2023-04-19 00:05:57 -0700

    added mido logic a bit

--- python_ver/test.py
+++ python_ver/test.py
@@ -1,10 +1,34 @@
-import pygame
+
 import time, sys
-from pygame import mixer
+from mido import MidiFile
 
-pygame.init()
-pygame.mixer.music.load('debussy-clair-de-lune.mid')
-pygame.mixer.music.play()
+def main():
+    print("jhi!")
 
-while pygame.mixer.music.get_busy():
-    continue
+    mid = MidiFile('debussy-clair-de-lune.mid')
+
+    notes = []
+
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
+        for msg in track:
+            if msg.type == 'note_on' and msg.velocity > 0 and msg.time > 10:
+                note = msg.note
+                time = msg.time
+                notes.append(('on', round(note_to_freq(note)), time * 0.01))
+                print("on  : ", round(note_to_freq(note)), time * 0.01)
+            elif msg.type == 'note_off' and msg.time > 10:
+                note = msg.note
+                time = msg.time
+                notes.append(('off', note_to_freq(note), time * 0.01))
+                print("off  : ", round(note_to_freq(note)), time * 0.01)
+
+
+    # TODO: convert these noteon events to triangle waves
+
+
+def note_to_freq(note):
+    return 440 * 2 ** ((note - 69) / 12)
+
+if __name__ == "__main__":
+    main()
